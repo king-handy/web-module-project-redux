f7fdee8ce7b23d1224bd9d68389a5178e413d744
Stephen Handy <a.king.life@pm.me>
2021-08-23 22:38:31 -0400

    MovieList map

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -13,17 +13,17 @@ const MovieList = (props)=> {
             <table className="table table-striped table-hover">
                 <thead>
                 <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
+                    <th>Title {props.title}</th>
+                    <th>Director {props.director}</th>
+                    <th>Genre {props.genre}</th>
+                    <th>Metascore {props.metascore}</th>
                     <th></th>
                 </tr>
                 </thead>
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        movies.map(movie=> (<MovieListItem key={props.movie.id} movie={props.movie}/>))
                     }
                 </tbody>
             </table>
@@ -35,7 +35,10 @@ const MovieList = (props)=> {
 
 const mapStateToProps = state => {
     return {
-        
+        title: state.title,
+        director: state.director,
+        genre: state.genre,
+        metascore: state.metascore
     }
 }
 
